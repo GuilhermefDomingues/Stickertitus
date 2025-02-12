4723c9b1121f5efd300bd9953a9d2532217d4d8c
GuilhermefDomingues <guilhermefrancadomingues@outlook.com>
2025-02-12 11:07:21 -0300

    chore: create and apply Button component

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,5 +1,6 @@
 import { Text, View, StyleSheet } from "react-native";
 import ImageViewer from "@/components/ImageViewer"; 
+import Button from "@/components/Button";
 
 const placeholderImage = require("@/assets/images/264292112-golden-retriever-1.jpg")
 
@@ -8,6 +9,10 @@ export default function Index() {
     <View style={styles.container}>
       <View style={styles.imageContainer}>
         <ImageViewer imgSource={placeholderImage} />
+      </View>
+      <View style={styles.footerContainer}>
+        <Button label="chose a photo"/>
+        <Button label="use this photo"/>
       </View>
     </View>
   );
@@ -22,6 +27,11 @@ const styles = StyleSheet.create({
   },
   imageContainer: {
     flex: 1,
+    paddingTop: 8,
+  },
+  footerContainer:{
+    flex: 1/3,
+    alignItems: "center",
   },
 
 });
